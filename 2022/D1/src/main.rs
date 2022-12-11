12b12be9281ce1a27ae31bea4c85b34851621350
Ghyslainnen <ghyslainnen@gmail.com>
2022-12-11 16:40:35 +0100

    2022-1-B

--- 2022/D1/src/main.rs
+++ 2022/D1/src/main.rs
@@ -1,12 +1,11 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-
 use std::{fs::File, io::Read};
 
 fn main() {
     let contents = read_file("input.txt").expect("Couldn't find file");
     let vec = sum_elf_calories(&contents);
 
-    part_a(vec);
+    part_a(vec.clone());
+    part_b(vec);
 }
 
 fn read_file(file: &str) -> std::io::Result<String> {
@@ -29,7 +28,6 @@ fn sum_elf_calories(contents: &str) -> Vec<i32> {
         let line: i32 = line.trim().parse().expect("Expected a number");
         num += line;
     }
-
     vec
 }
 
@@ -42,4 +40,25 @@ fn part_a(vec: Vec<i32>) {
         }
     }
     println!("Part A = {prev_n}");
+}
+
+fn part_b(vec: Vec<i32>) {
+    let mut top_one = 0;
+    let mut top_two = 0;
+    let mut top_three = 0;
+
+    for n in vec {
+        if n > top_one {
+            top_three = top_two;
+            top_two = top_one;
+            top_one = n;
+        } else if n > top_two {
+            top_three = top_two;
+            top_two = n;
+        } else if n > top_three {
+            top_three = n;
+        }
+    }
+
+    println!("Part B = {}", top_one + top_two + top_three);
 }
