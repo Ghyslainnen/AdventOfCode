50a7d01e4ba45caaf7a7ad8e8146c056b62b96c1
Ghyslainnen <ghyslainnen@gmail.com>
2022-12-12 22:04:15 +0100

    2022-2-B

--- 2022/D2/src/main.rs
+++ 2022/D2/src/main.rs
@@ -1,28 +1,35 @@
 // A and X for Rock, B and Y for Paper, C and Z for Scissors
 fn main() -> std::io::Result<()> {
-    let mut score: u64 = 0;
+    let mut score_a = 0;
+    let mut score_b = 0;
     let text = std::fs::read_to_string("input.txt")?;
+
+    score_a += compute_choice_points_part_a(&text);
+
+    score_a += part_a(&text);
+    println!("Part A = {}", score_a);
+    score_b += part_b(&text);
+    println!("Part B = {}", score_b);
+    Ok(())
+}
+
+fn compute_choice_points_part_a(text: &str) -> u64 {
+    let mut score = 0;
 
     for line in text.lines() {
         if line.contains('X') {
             score += 1;
         } else if line.contains('Y') {
             score += 2;
-        } else {
+        } else if line.contains('Z') {
             score += 3;
         }
     }
-    score += compute_single_game_result(&text);
-    println!("{}", score);
-    Ok(())
+
+    score
 }
 
-/// First if statement calculates draws (3 points)
-///
-/// Second if statement calculates wins (6 points)
-///
-/// No loses calculation because you don't win any points
-fn compute_single_game_result(text: &str) -> u64 {
+fn part_a(text: &str) -> u64 {
     let mut score: u64 = 0;
     for line in text.lines() {
         if (line.contains('A') && line.contains('X'))
@@ -35,6 +42,37 @@ fn compute_single_game_result(text: &str) -> u64 {
             || (line.contains('C') && line.contains('X'))
         {
             score += 6;
+        }
+    }
+    score
+}
+fn part_b(text: &str) -> u64 {
+    let mut score: u64 = 0;
+    for line in text.lines() {
+        if line.contains('A') {
+            if line.contains('X') {
+                score += 3;
+            } else if line.contains('Y') {
+                score = score + 3 + 1;
+            } else {
+                score = score + 6 + 2;
+            }
+        } else if line.contains('B') {
+            if line.contains('X') {
+                score += 1;
+            } else if line.contains('Y') {
+                score = score + 3 + 2;
+            } else {
+                score = score + 6 + 3;
+            }
+        } else if line.contains('C') {
+            if line.contains('X') {
+                score += 2;
+            } else if line.contains('Y') {
+                score = score + 3 + 3;
+            } else {
+                score = score + 6 + 1;
+            }
         }
     }
     score
